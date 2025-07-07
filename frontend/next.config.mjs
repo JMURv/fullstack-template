ae70542bbd7f5941178559459546ab633b05b9b3
JMURv <architect.lock@outlook.com>
2025-07-08 00:34:48 +0300

    add frontend Dockerfiles

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    output: 'standalone',
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            { hostname: 'localhost' },
+            { hostname: '127.0.0.1' },
+            { hostname: 'caddy' },
+        ],
+    },
+    async rewrites() {
+        const backendURL = process.env.BACKEND_URL || 'http://localhost:8080';
+        const s3URL = process.env.S3_URL || 'http://localhost:9000';
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${backendURL}/:path*`,
+            },
+            {
+                source: '/s3/:path*',
+                destination: `${s3URL}/:path*`,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
